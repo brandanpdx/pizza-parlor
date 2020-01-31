c97bd50ab983e60b9a113b415814b768d7242627
Brandan Sayarath <bsayarath@gmail.com>
2020-01-31 11:02:20 -0800

    refactor HTML and JS due to naming convention bug

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Pizza(size, crust, toppings) {
   this.toppings = toppings;
 }
 
-Pizza.prototype.sizePricing = function(size) {
+Pizza.prototype.sizePricing = function() {
   if (this.size === "small") {
     price = 10;
   } else if (this.size === "medium") {
@@ -13,12 +13,29 @@ Pizza.prototype.sizePricing = function(size) {
   } else if (this.size === "large") {
     price = 20;
   }
+  return price;
 }
 
 //User Logic
+var pie = new Pizza();
 
-var pie = new pizza();
 
-$(document).ready(function(){
+$(document).ready(function() {
+  $(".pizzamaker").submit(function() {
+    event.preventDefault();
+    var pizzaSize = $("#piesize").val();
+    var pizzaCrust = $("#piecrust").val();
+
+    console.log(pizzaSize);
+    console.log(pizzaCrust);
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      var pizzaToppings = $(this).val();
+      $("#showtoppings").append(pizzaToppings + "<br>");
+    });
+
+
+
+
+  })
 
 })
