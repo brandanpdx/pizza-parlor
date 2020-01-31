6ae441bd0f8e7fb4d970ad3c9d884b720b0aa6ff
Brandan Sayarath <bsayarath@gmail.com>
2020-01-31 09:47:30 -0800

    add initial javascript business logic functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
+//Business logic
+function Pizza(size, crust, toppings) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+}
 
+Pizza.prototype.sizePricing = function(size) {
+  if (this.size === "small") {
+    price = 10;
+  } else if (this.size === "medium") {
+    price = 16;
+  } else if (this.size === "large") {
+    price = 20;
+  }
+}
+
+//User Logic
+
+var pie = new pizza();
+
+$(document).ready(function(){
+
+})
