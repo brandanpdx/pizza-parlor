fdf110e80977acffeb3b7a519614f649722f5dfb
Brandan Sayarath <bsayarath@gmail.com>
2020-01-31 11:55:14 -0800

    add pricing details for premium meats

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,46 @@
 //Business logic
+
+//Pizza Constructor
 function Pizza(size, crust, toppings) {
   this.size = size;
   this.crust = crust;
   this.toppings = toppings;
 }
-
-Pizza.prototype.sizePricing = function() {
+//Pricing Prototype
+Pizza.prototype.pricing = function() {
+  var price = 0 
   if (this.size === "small") {
-    price = 10;
+    price += 10;
   } else if (this.size === "medium") {
-    price = 16;
+    price += 15;
   } else if (this.size === "large") {
-    price = 20;
+    price += 20;
+  }
+  if (this.toppings.includes("proscuitto") || this.toppings.includes("salami")) {
+    price += 2
   }
   return price;
 }
 
 //User Logic
-var pie = new Pizza();
 
+function showOrderDetails = function(pizza) {
+  
+}
 
 $(document).ready(function() {
   $(".pizzamaker").submit(function() {
     event.preventDefault();
-    var pizzaSize = $("#piesize").val();
-    var pizzaCrust = $("#piecrust").val();
+    var userSize = $("#piesize").val();
+    var userCrust = $("#piecrust").val();
+    var chosenToppings = []
+    var pie = new Pizza(userSize,userCrust,chosenToppings);
 
-    console.log(pizzaSize);
-    console.log(pizzaCrust);
     $("input:checkbox[name=toppings]:checked").each(function() {
-      var pizzaToppings = $(this).val();
-      $("#showtoppings").append(pizzaToppings + "<br>");
+      var selectedToppings = $(this).val();
+      chosenToppings.push(selectedToppings);
+
+
     });
-
-
-
-
   })
-
 })
